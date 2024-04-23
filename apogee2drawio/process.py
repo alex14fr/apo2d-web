e8f4143e2bf486863975495bd952a0aab4edf483
Pavel Kalugin <pkfrance@yahoo.com>
2024-04-23 19:23:00 +0200

    Make the package callable from the command line

---
+++ apogee2drawio/process.py
@@ -0,0 +1,18 @@
+import argparse
+from .diagram_maker import make_diagram
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Process Apogée XML to DrawIO diagram')
+    parser.add_argument('-in', '--infile', type=str, help='Input apogée XML file path')
+    parser.add_argument('-out', '--outfile', type=str, help='Output DrawIO file path')
+
+    args = parser.parse_args()
+
+    if args.infile is None or args.outfile is None:
+        parser.error("Please provide both input and output file paths.")
+    else:
+        make_diagram(args.infile, args.outfile)
+
+if __name__ == "__main__":
+    main()

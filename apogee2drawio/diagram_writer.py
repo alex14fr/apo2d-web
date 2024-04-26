5d0c539dca8ad8d0ae73e9d239073ccc8539ced9
Pavel Kalugin <pkfrance@yahoo.com>
2024-04-26 21:37:06 +0200

    Switching to the fixed width fornt

--- apogee2drawio/diagram_writer.py
+++ apogee2drawio/diagram_writer.py
@@ -6,13 +6,13 @@ HIGHT_ITEM=26
 
 STYLE_HEADER="swimlane;fontStyle=0;childLayout=stackLayout;horizontal=1;\
     startSize=30;horizontalStack=0;resizeParent=1;resizeParentMax=0;resizeLast=0;\
-        collapsible=0;marginBottom=0;portConstraintRotation=0;rotatable=0;dropTarget=0;resizable=0;"
+        collapsible=0;marginBottom=0;portConstraintRotation=0;rotatable=0;dropTarget=0;resizable=0;fontFamily=Courier New;"
 STYLE_ITEM="text;strokeColor=none;fillColor=none;align=left;verticalAlign=middle;\
     spacingLeft=4;spacingRight=4;overflow=hidden;points=[[0,0.5],[1,0.5]];\
-        portConstraint=eastwest;rotatable=0;locked=1;"
+        portConstraint=eastwest;rotatable=0;locked=1;fontFamily=Courier New;"
 STYLE_SUSPENDED_ITEM="text;strokeColor=none;fillColor=none;align=left;verticalAlign=middle;\
     spacingLeft=4;spacingRight=4;overflow=hidden;points=[[0,0.5],[1,0.5]];\
-        portConstraint=eastwest;rotatable=0;locked=1;fontStyle=2;fontColor=#CCCCCC;"
+        portConstraint=eastwest;rotatable=0;locked=1;fontStyle=2;fontColor=#CCCCCC;fontFamily=Courier New;"
 STYLE_LINK="edgeStyle=segmentEdgeStyle;endArrow=classic;html=1;curved=0;rounded=1;\
     endSize=8;startSize=8;locked=1;"
 STYLE_LABEL="edgeLabel;resizable=0;html=1;align=center;verticalAlign=middle;"

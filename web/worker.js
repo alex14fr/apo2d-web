df72dabdf2e9f3117a64e904ff837d69b9a9cac9
Alexandre Janon <alexandre.janon@universite-paris-saclay.fr>
2024-04-27 15:43:58 +0200

    web: show option

--- web/worker.js
+++ web/worker.js
@@ -5,13 +5,13 @@ importScripts("https://cdn.jsdelivr.net/pyodide/v0.25.1/full/pyodide.js");
 async function loadpy() {
 	pyo=await loadPyodide();
 	pyo.unpackArchive(await (await fetch("pkg.tgz")).arrayBuffer(), "tgz");
-	pyo.runPython("import apogee2drawio");
+	pyo.runPython("import apogee2drawio; from apogee2drawio.diagram_writer import ShowOption");
 	self.postMessage("ready");
 }
 
 function gotmsg(ev) {
 	pyo.FS.writeFile("tmp.xml", ev.data);
-	pyo.runPython("apogee2drawio.make_diagram('tmp.xml','tmp2.xml')")
+	pyo.runPython("apogee2drawio.make_diagram('tmp.xml','tmp2.xml',ShowOption.CODE_AND_DESCRIPTION)")
 	var xmlstr=pyo.FS.readFile("tmp2.xml", {"encoding":"utf8"});
 	var msgstr=JSON.stringify({"action":"load", "xml":xmlstr});
 	self.postMessage(msgstr);
